89208bed2803be7971779d1cb71f3387204b218a
damachad <davidmachado98@hotmail.com>
2023-12-12 11:32:49 +0000

    Created files

---
+++ philo/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: damachad <damachad@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/12 11:15:11 by damachad          #+#    #+#             */
+/*   Updated: 2023/12/12 11:22:14 by damachad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <string.h>
+# include <sys/time.h>
+
+#endif
